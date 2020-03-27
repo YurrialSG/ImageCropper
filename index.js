01fbd78c4e2ee8065cd32d2f3fdb615ac28a609a
YurrialSG <yuriinternacional86@gmail.com>
2020-03-27 16:34:45 -0300

    crop com botoes de açao

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ let cropper = null;
 let myGreatImage = null;
 const croppedImage = document.getElementById("croppedImage");
 
+const move = document.getElementById("move");
+move.addEventListener("click", mover);
+
+const crop = document.getElementById("crop");
+crop.addEventListener("click", cortar);
+
+const maisZoomButton = document.getElementById("maisZoom");
+maisZoomButton.addEventListener("click", maisZoom);
+
+const menosZoomButton = document.getElementById("menosZoom");
+menosZoomButton.addEventListener("click", menosZoom);
+
 const cropButton = document.getElementById("cropButton");
 cropButton.addEventListener("click", cropImage);
 
@@ -26,14 +38,14 @@ function getImage() {
 function processImage() {
   cropper = new Cropper(myGreatImage, {
     viewMode: 1, //restringe a caixa de corte para não exceder o tamanho da tela
-    autoCropArea: 0.85,
-    wheelZoomRatio: 0.02,
-    restore: false,
-    guides: false,
-    highlight: false,
-    cropBoxMovable: false,
-    aspectRatio: 20 / 10, //define o tamanho de área onde vai ser recortada
-    zoom: function (e) { // function to allow maxZoom to max size
+    autoCropArea: 0.35, //define o tamanho da área de corte automático 'porcentagem' 0 -> 1
+    wheelZoomRatio: 0.02, //define a taxa de zoom ao ampliar a imagem girando o mouse
+    restore: false, //Restaure a área cortada após redimensionar a janela
+    guides: false, //Mostre as linhas tracejadas acima da caixa de corte
+    highlight: true, //Mostre o modal branco acima da caixa de corte 'realce a caixa de corte'
+    cropBoxMovable: false, //Ative para mover a caixa de corte arrastando.
+    aspectRatio: 30 / 10, //define o tamanho de área onde vai ser recortada
+    zoom: function (e) { // função para permitir maxZoom ao tamanho máximo
       if (e.ratio > 1) {
         e.preventDefault();
         $(this).cropper('zoomTo', 1);
@@ -60,6 +72,22 @@ function cropImage() {
   img.src = imgurl;
   document.getElementById("cropResult").appendChild(img);
   saveImage()
+}
+
+function mover() {
+  cropper.setDragMode('move');
+}
+
+function cortar() {
+  cropper.setDragMode('crop')
+}
+
+function menosZoom() {
+  cropper.zoom(-0.1);
+}
+
+function maisZoom() {
+  cropper.zoom(0.1);
 }
 
 function saveImage() {
