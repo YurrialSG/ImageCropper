82ee7dd4ce775fdeba4a527acfa469617deb593c
YurrialSG <yuriinternacional86@gmail.com>
2020-03-27 17:35:06 -0300

    area do crop fixa

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ menosZoomButton.addEventListener("click", menosZoom);
 const cropButton = document.getElementById("cropButton");
 cropButton.addEventListener("click", cropImage);
 
+const disableButton = document.getElementById("lock");
+disableButton.addEventListener("click", lock);
+
+const enableButton = document.getElementById("unlock");
+enableButton.addEventListener("click", unlock);
+
 function getImage() {
   // console.log("images", this.files[0]);
   const imageToProcess = this.files[0];
@@ -38,20 +44,29 @@ function getImage() {
 function processImage() {
   cropper = new Cropper(myGreatImage, {
     viewMode: 1, //restringe a caixa de corte para não exceder o tamanho da tela
+    dragMode: 'move', //Você pode alternar entre os modos "crop" e "move".
     autoCropArea: 0.35, //define o tamanho da área de corte automático 'porcentagem' 0 -> 1
     wheelZoomRatio: 0.02, //define a taxa de zoom ao ampliar a imagem girando o mouse
     restore: false, //Restaure a área cortada após redimensionar a janela
     guides: false, //Mostre as linhas tracejadas acima da caixa de corte
     highlight: true, //Mostre o modal branco acima da caixa de corte 'realce a caixa de corte'
     cropBoxMovable: false, //Ative para mover a caixa de corte arrastando.
+    cropBoxResizable: true, //Ative para redimensionar a caixa de corte arrastando.
     aspectRatio: 30 / 10, //define o tamanho de área onde vai ser recortada
+    minCropBoxWidth: 150, //minimo de largura
+    minCropBoxHeight: 50, //minimo de altura
+    toggleDragModeOnDblclick: false, //Ative para alternar o modo de arrastar entre "cortar" e "mover" ao clicar duas vezes no cortador.
+    built: function () {
+      // Width and Height params are number types instead of string
+      $toCrop.cropper("setCropBoxData", { width: 1600, height: 800 });
+    },
     zoom: function (e) { // função para permitir maxZoom ao tamanho máximo
       if (e.ratio > 1) {
         e.preventDefault();
         $(this).cropper('zoomTo', 1);
       }
     },
-
+    
     crop(event) {
       // console.log(event.detail.x);
       // console.log(event.detail.y);
@@ -100,4 +115,12 @@ function saveImage() {
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
+}
+
+function lock() {
+  cropper.disable()
+}
+
+function unlock() {
+  cropper.enable()
 }
